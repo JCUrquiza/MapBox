ba3743d8d87d5d0bf1eb0ea15b9e03156e5d44b1
Juan <gurquiza8@gmail.com>
2024-05-19 14:53:43 -0500

    Convert side menu to standalone component

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   standalone: true,
   imports: [
     CommonModule,
-    CounterAloneComponent
+    CounterAloneComponent,
+    SideMenuComponent
   ],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
