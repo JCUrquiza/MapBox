ba81c062bc7548e8ca2bee51fa56efb61ef6aabb
Juan <gurquiza8@gmail.com>
2024-05-18 22:35:09 -0500

    upgrade marker location

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -69,6 +69,10 @@ export class MarkersPageComponent implements AfterViewInit {
     });
 
     this.saveToLocalStorage();
+
+    marker.on('dragend', () => {
+      this.saveToLocalStorage();
+    });
   }
 
   deleteMarker( index: number ) {
